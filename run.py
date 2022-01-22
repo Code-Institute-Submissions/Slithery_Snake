327c528d23aaee2d0fe3df47fc406bd2c535ce81
EmmaNoelles <emmas2810@gmail.com>
2022-01-22 21:21:07 +0000

    added functions to set up the screen

--- run.py
+++ run.py
@@ -8,3 +8,10 @@ delay = 0.1
 # Score
 score = 0
 high_score = 0
+
+# Set up the screen
+wn = turtle.Screen()
+wn.title("Sneaky Snake")
+wn.bgcolor("blue")
+wn.setup(width=600, height=600)
+wn.tracer(0)
