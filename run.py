d98c6316509766f8bbde5d60542d6ec849bc2b05
EmmaNoelles <emmas2810@gmail.com>
2022-01-22 22:00:13 +0000

    added in Snake Head functions that i forgot to add

--- run.py
+++ run.py
@@ -16,6 +16,15 @@ wn.bgcolor("blue")
 wn.setup(width=600, height=600)
 wn.tracer(0)
 
+# Snake head
+head = turtle.Turtle()
+head.speed(0)
+head.shape("square")
+head.color("black")
+head.penup()
+head.goto(0,0)
+head.direction = "stop"
+
 # Snake food
 food = turtle.Turtle()
 food.speed(0)
@@ -34,24 +43,28 @@ pen.color("white")
 pen.penup()
 pen.hideturtle()
 pen.goto(0, 260)
-pen.write("Score: 0  High Score: 0", align="center", font=("Courier", 24, "normal"))
+pen.write("Score: 0  High Score: 0", align="center")
 
 # Functions
-def go_up():
+def go_up():  
     if head.direction != "down":
         head.direction = "up"
+
 
 def go_down():
     if head.direction != "up":
         head.direction = "down"
 
+
 def go_left():
     if head.direction != "right":
         head.direction = "left"
 
+
 def go_right():
     if head.direction != "left":
         head.direction = "right"
+
 
 def move():
     if head.direction == "up":
