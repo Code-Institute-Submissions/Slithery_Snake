0d149e68530ae12c152f5434dd7ea29931d80a93
EmmaNoelles <emmas2810@gmail.com>
2022-01-22 21:23:00 +0000

    added pen functions

--- run.py
+++ run.py
@@ -25,3 +25,13 @@ food.penup()
 food.goto(0,100)
 
 segments = []
+
+# Pen
+pen = turtle.Turtle()
+pen.speed(0)
+pen.shape("square")
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write("Score: 0  High Score: 0", align="center", font=("Courier", 24, "normal"))
